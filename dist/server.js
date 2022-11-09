4c9d212e9a8d00d50c5a054943adfc5cb7f400e0
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-11-08 23:49:58 -0300

    enviroment.d.ts deleted

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 var server = express();
 server.use(cors());
 server.listen(process.env.PORT || 5000, function () {
